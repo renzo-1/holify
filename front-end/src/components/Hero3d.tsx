ca5b0175fb66ad7a772633d90adc89719320a3f0
renzo-1 <renzosarceo@gmail.com>
2023-11-14 22:16:35 +0800

    added 3d hero

--- front-end/src/components/Hero3d.tsx
+++ front-end/src/components/Hero3d.tsx
@@ -34,7 +34,7 @@ function Diploma(props: JSX.IntrinsicElements["group"]) {
         receiveShadow
         geometry={nodes.diploma1.geometry}
         material={nodes.diploma1.material}
-        scale={0.2}
+        scale={-10}
       />
     </group>
   );
